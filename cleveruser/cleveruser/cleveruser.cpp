5558fc48b7d49a5008d615b5390f9f871b332afc
Protiwi1337 <146383409+Protiwi1337@users.noreply.github.com>
2023-09-28 19:51:36 +0000

    Update cleveruser.cpp

--- cleveruser/cleveruser/cleveruser.cpp
+++ cleveruser/cleveruser/cleveruser.cpp
@@ -7,14 +7,18 @@ int main()
 {
     system("title cantidebug usermode test.exe");
     printf("[+] sent innit request !\n");
+    //calling innit code that will start the debugengine
     cdebug::innit();
     Sleep(1000);
     printf("[+] sent dbgkill request !\n");
+    //calling debugkill code that will send request to the clever engine to kill all known debug programs
     cdebug::debugkill();
     Sleep(1000);
     printf("[+] sent dbgcheck request !\n");
+    //call debugcheck cod that will send request to the clever enginge to check for debugger drivers that are loaded
     cdebug::debugcheck();
     Sleep(1000);
+    cdebug::servicekill();
     system("pause >nul");
 }
 
